c81fd9c62e85469f5d00f34dd05d2ebf5bb9f776
Emeric <emeric.grange@gmail.com>
2026-06-29 16:45:34 +0200

    Bump version to 0.18

--- deploy_linux.sh
+++ deploy_linux.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 
 export APP_NAME="toolBLEx"
-export APP_VERSION=0.17
+export APP_VERSION=0.18
 export GIT_VERSION=$(git rev-parse --short HEAD)
 
 #export APP_NAME_LOWERCASE=${APP_NAME,,}  # lowercase
